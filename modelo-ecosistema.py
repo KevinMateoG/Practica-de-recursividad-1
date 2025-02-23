c1d5ef5afa63fced97eb31e72f48db0f4e2ec1f9
Kevin Mateo Gomez <gomezdiezkevinmateo@gmail.com>
2025-02-23 16:18:29 -0500

    correccion de errores

--- modelo-ecosistema.py
+++ modelo-ecosistema.py
@@ -3,7 +3,6 @@ import random
 class Presa:
     def __init__(self, animal_presa):
         self.animal_presa: str = animal_presa
-        self.energia: int = 50
 
     def alimentarse(self):
         pass
@@ -34,11 +33,26 @@ def crear_matriz(n: int, i:int = 0, j:int = 0,  fila:list = [],matriz: list[list
     fila.append("_")
     return crear_matriz(n, i, j + 1, fila, matriz)
 
+def agregar_al_ecosistema(matriz: list, cont: int =0):
+    limite = len(matriz) - 1
+    i = random.randint(0, limite)
+    j = random.randint(0, limite)
+    if cont > len(matriz):
+        return matriz
+    
+    if matriz[i][j] == "_":
+        matriz[i][j] = "P"
+    
+    if matriz[i][j] == "P":
+        pass
+
+    return agregar_presas(matriz, cont + 1)
+
 def agregar_presas(matriz: list, cont_presas: int = 0):
     limite = len(matriz) - 1
     i = random.randint(0, limite)
     j = random.randint(0, limite)
-    if cont_presas > len(matriz):
+    if cont_presas == len(matriz):
         return matriz
     if matriz[i][j] == "_":
         matriz[i][j] = "P"
@@ -51,13 +65,18 @@ def agregar_depredadores(matriz: list, cont_depredadores: int = 0):
     limite = len(matriz) - 1
     i = random.randint(0, limite)
     j = random.randint(0, limite)
-    if cont_depredadores > len(matriz):
+    if cont_depredadores == len(matriz):
         return matriz
+    if matriz[i][j] == "P" or "D":
+        pass
+    
     if matriz[i][j] == "_":
         matriz[i][j] = "D"
+    
     return agregar_depredadores(matriz, cont_depredadores+1)
 
-ecosistema = crear_matriz(7)
-presa = agregar_presas(ecosistema)
+ecosistema = crear_matriz(3)
 depredador = agregar_depredadores(ecosistema)
+presa = agregar_presas(ecosistema)
+
 print(ecosistema)
